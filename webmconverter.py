49e63c32d3c5fc4915c57ca65df582ee52f1c9a1
S Samuel <shsamuel92@gmail.com>
2022-07-13 04:53:01 -0600

    Changed to camel casing Creating folder structure Adding new lines for readbility

--- webmconverter.py
+++ webmconverter.py
@@ -2,70 +2,75 @@ import os
 import subprocess
 import sys
 
-dropped_file = sys.argv[1]
+inputVideo = sys.argv[1]
 
-def convert_to_webm(inputFile):
-    file_size_input = ""
-    file_name_input = ""
-    file_duration_input = ""
+def convertToWebm(inputFile):
+    outputFileSize = ""
+    outputFileName = ""
+    inputVideoDuration = ""
     height = ""
     width = ""
 
-    while(has_numbers(file_size_input) == False):
-        file_size_input = input("Enter Output Video Size (In MB): ")
+    while(hasNumbers(outputFileSize) == False):
+        outputFileSize = input("Enter Output Video Size (In MB): \n")
+        print("\n")
 
-    while(has_numbers(file_duration_input) == False):
-        file_duration_input = input("Enter Duration (In SECONDS): ")
+    while(hasNumbers(inputVideoDuration) == False):
+        inputVideoDuration = input("Enter Duration (In SECONDS): \n")
+        print("\n")
 
-    while((file_name_input) == ""):
-        file_name_input = input("Enter Output Filename: ")
+    while((outputFileName) == ""):
+        outputFileName = input("Enter Output Filename: \n")
+        print("\n")
 
-    while(has_numbers(width) == False):
-        width = input("Enter Output Width (Default: 1280): ")
+    while(hasNumbers(width) == False):
+        width = input("Enter Output Width (Default: 1280): \n")
+        print("\n") 
         if(width == ""):
             width = "1280"
 
-    while(has_numbers(height) == False):
+    while(hasNumbers(height) == False):
         height = input("Enter Output Height (Default: -1 To Maintain Aspect" 
-                       " Ratio): ")
+                       " Ratio): \n")
+        print("\n")
         if(height == ""):
             height = "-1"
 
-    if ".webm" in file_name_input:
+    if ".webm" in outputFileName:
         pass
     else:
-        file_name_input += ".webm"
+        outputFileName += ".webm"
 
-    video_size = int(file_size_input)
-    video_seconds = int(file_duration_input)
-    bitrate = int(((video_size * 1024 * 8) / video_seconds))
+    videoSize = int(outputFileSize)
+    outputVideoDuration = int(inputVideoDuration)
+    bitrate = int(((videoSize * 1024 * 8) / outputVideoDuration))
     
     print(f"{width} x {height} Video Resolution")
     print((bitrate), " KB/s")
-    print(file_name_input)
+    print((outputFileName), "\n\n")
 
-    start_button = input("Enter To Start Conversion. Enter 'Q' to Quit. ")
-    if(start_button.lower() == "q"):
+    startButton = input("Enter To Start Conversion. Enter 'Q' to Quit. ")
+    if(startButton.lower() == "q"):
         return 0
 
-    subprocess.call(["ffmpeg.exe", '-i', f'{dropped_file}', '-c:v', 
+    subprocess.call(["ffmpeg.exe", '-i', f'{inputVideo}', '-c:v', 
                      'libvpx-vp9', '-pass', '1', '-b:v', f'{bitrate}K', 
                      '-threads', '16', '-speed', '0', '-vf', 
                      f'scale={width}:{height}', '-tile-columns', '0', 
                      '-frame-parallel', '0', '-auto-alt-ref', '1', 
                      '-lag-in-frames', '25', '-g', '600', '-aq-mode', '0', 
-                     '-an', '-f', 'webm',f'{file_name_input}'])
+                     '-an', '-f', 'webm',f'.\WebM\{outputFileName}'])
 
-    subprocess.call(["ffmpeg.exe", '-i', f'{dropped_file}', '-c:v', 
+    subprocess.call(["ffmpeg.exe", '-i', f'{inputVideo}', '-c:v', 
                      'libvpx-vp9', '-pass', '2', '-b:v', f'{bitrate}K', 
                      '-threads', '16', '-speed', '0', '-vf', 
                      f'scale={width}:{height}', '-tile-columns', '0', 
                      '-frame-parallel', '0', '-auto-alt-ref', '1', 
                      '-lag-in-frames', '25', '-g', '600', '-aq-mode', '0', 
-                     '-an', '-f', 'webm',f'{file_name_input}'])
+                     '-an', '-f', 'webm',f'.\WebM\{outputFileName}'])
     pass
 
-def has_numbers(inputString):
+def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
 
-convert_to_webm(dropped_file)
+convertToWebm(inputVideo)
