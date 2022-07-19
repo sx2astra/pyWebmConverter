b2f45ca8860d7e83b1de09c18d56d7fac7b8d3fc
S Samuel <shsamuel92@gmail.com>
2022-07-18 21:21:44 -0600

    All major functions split (#4)

--- pyWebMConverter.py
+++ pyWebMConverter.py
@@ -1,50 +1,24 @@
 import os
 import subprocess
 import sys
+import unittest
 
 inputVideo = sys.argv[1]
 
 def convertToWebm(inputFile):
-    outputFileSize = ""
+    bitrate = calculateBitrate()
+    width, height = setResolution()
+    #startTime, endTime = trimVideo()
+    #cropW, cropH, cropX, cropY = cropVideo()
+
     outputFileName = ""
-    inputVideoDuration = ""
-    height = ""
-    width = ""
-    videoPass = 1
-
-    while(hasNumbers(outputFileSize) == False):
-        outputFileSize = input("Enter Output Video Size (In MB): \n")
-        print("\n")
-
-    while(hasNumbers(inputVideoDuration) == False):
-        inputVideoDuration = input("Enter Duration (In SECONDS): \n")
-        print("\n")
-
     while((outputFileName) == ""):
         outputFileName = input("Enter Output Filename: \n")
-        print("\n")
-
-    while(hasNumbers(width) == False):
-        width = input("Enter Output Width (Default: 1280): \n")
-        print("\n") 
-        if(width == ""):
-            width = "1280"
-
-    while(hasNumbers(height) == False):
-        height = input("Enter Output Height (Default: -1 To Maintain Aspect" 
-                       " Ratio): \n")
-        print("\n")
-        if(height == ""):
-            height = "-1"
 
     if ".webm" in outputFileName:
         pass
     else:
         outputFileName += ".webm"
-
-    videoSize = int(outputFileSize)
-    outputVideoDuration = int(inputVideoDuration)
-    bitrate = int(((videoSize * 1024 * 8) / outputVideoDuration))
     
     print(f"{width} x {height} Video Resolution")
     print((bitrate), " KB/s")
@@ -54,6 +28,7 @@ def convertToWebm(inputFile):
     if(startButton.lower() == "q"):
         return 0
 
+    videoPass = 1
     while(videoPass <= 2):
         subprocess.call(["ffmpeg.exe", '-i', f'{inputVideo}', '-c:v', 
                          'libvpx-vp9', '-pass', f'{videoPass}', '-b:v', 
@@ -69,6 +44,70 @@ def convertToWebm(inputFile):
     print("Conversion Complete!\n")
     input(f"File Avaliable at .\Output\{outputFileName}")
     pass
+
+def trimVideo():
+    startTime = ""
+    endTime = ""
+    while(hasNumbers(startTime) == False):
+        startTime = input("Enter Video Start Time (HH:MM:SS / Secs): ")
+
+    while(hasNumbers(endTime) == False):
+        endTime = input("Enter Video End Time (HH:MM:SS / Secs): ")
+
+    return startTime, endTime
+
+def cropVideo():
+    cropW, cropH, cropX, cropY = "0"
+    switcher = input("What type of crop?\n"
+                     "0: None\n"
+                     "1: To 4:3\n"
+                     "2: To 16:9\n"
+                     "3: From Top Right\n"
+                     "4: From Center\n"
+                     "5: Custom\n"
+                    )
+
+    switcher = {
+        0: "pass",
+        1: "crop=ih/3*4:ih",
+        2: "crop=ih/16*9:ih",
+        3: ""
+        }
+    return switcher.get(num, "Invalid Input")
+
+def calculateBitrate():
+    outputFileSize = ""
+    inputVideoDuration = ""
+    while(hasNumbers(outputFileSize) == False):
+        outputFileSize = input("Enter Output Video Size (In MB): \n")
+        print("\n")
+
+    while(hasNumbers(inputVideoDuration) == False):
+        inputVideoDuration = input("Enter Duration (In SECONDS): \n")
+        print("\n")
+
+    videoSize = int(outputFileSize)
+    outputVideoDuration = int(inputVideoDuration)
+    bitrate = int(((videoSize * 1024 * 8) / outputVideoDuration))
+    return bitrate
+
+def setResolution():
+    width = ""
+    height = ""
+    while(hasNumbers(width) == False):
+        width = input("Enter Output Width (Default: 1280): \n")
+        print("\n") 
+        if(width == ""):
+            width = "1280"
+
+    while(hasNumbers(height) == False):
+        height = input("Enter Output Height (Default: -1 To Maintain Aspect" 
+                       " Ratio): \n")
+        print("\n")
+        if(height == ""):
+            height = "-1"
+
+    return width, height
 
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
