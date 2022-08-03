d2735ca36e97ffcf438417ded0b272c349928a60
S Samuel <shsamuel92@gmail.com>
2022-08-02 20:58:39 -0600

    Cleaned up debug statements, final action test

--- test/test_basic.py
+++ test/test_basic.py
@@ -2,21 +2,10 @@ import pytest
 import sys
 import os
 
-
 try:
     sys.path.append("pyWebmConverter")
     from converter import *
 except ModuleNotFoundError:
-    print("Here")
-    path = "./"
-    print(os.listdir(path))
-    os.chdir("/home/runner/work/pyWebmConverter/pyWebmConverter/pyWebMConverter")
-    sys.path.insert(0, "/home/runner/work/pyWebmConverter/pyWebmConverter/pyWebMConverter")
-    print(sys.path)
-    print(os.listdir(path))
-    from converter import *
-else:
-    print("There")
     sys.path.insert(0, "/home/runner/work/pyWebmConverter/pyWebmConverter/pyWebMConverter")
     from converter import *
 finally:
@@ -33,7 +22,6 @@ def testCalculateBitrate():
     assert webMConverter.calculateBitrate(10,10) == 8192
     assert webMConverter.calculateBitrate("10","10") == 8192
     assert webMConverter.calculateBitrate("a","b") == 8192
-
 
 def testSetFileName():
     assert webMConverter.setFileName("abc") == "abc.webm"
