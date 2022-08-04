5a54648ecb70a7f4ac84b0df58e161288048cce3
S Samuel <shsamuel92@gmail.com>
2022-08-04 01:45:13 -0600

    Pylint Request

--- test/test_basic.py
+++ test/test_basic.py
@@ -13,19 +13,19 @@ finally:
 
 xfail = pytest.mark.xfail
 
-def testHasNumbers():
-    assert hasNumbers("5") == True
-    assert hasNumbers("a") == False
+def test_has_numbers():
+    assert has_numbers("5") == True
+    assert has_numbers("a") == False
 
 @xfail(raises=ValueError)
-def testCalculateBitrate():
-    assert webMConverter.calculateBitrate(10,10) == 8192
-    assert webMConverter.calculateBitrate("10","10") == 8192
-    assert webMConverter.calculateBitrate("a","b") == 8192
+def test_calculate_bitrate():
+    assert WebmConverter.calculate_bitrate("", 10,10) == 8192
+    assert WebmConverter.calculate_bitrate("", "10","10") == 8192
+    assert WebmConverter.calculate_bitrate("", "a","b") == 8192
 
-def testSetFileName():
-    assert webMConverter.setFileName("abc") == "abc.webm"
-    assert webMConverter.setFileName("abc.webm") == "abc.webm"
+def test_set_file_name():
+    assert WebmConverter.set_file_name("", "abc") == "abc.webm"
+    assert WebmConverter.set_file_name("", "abc.webm") == "abc.webm"
 
-if __name__ == 'test_pyWebMConverter()':
-    test_pyWebMConverter()
+if __name__ == 'test_basic()':
+    test_basic()
