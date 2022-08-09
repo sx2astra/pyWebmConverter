250e475c29a606637bc57622b19a2f520b95ffd7
S Samuel <shsamuel92@gmail.com>
2022-08-09 05:14:52 -0600

    Trying to parse ini

--- test/test_basic.py
+++ test/test_basic.py
@@ -33,5 +33,31 @@ def test_set_file_name():
     assert WebmConverter.set_file_name("", "abc") == "abc.webm"
     assert WebmConverter.set_file_name("", "abc.webm") == "abc.webm"
 
+def test_parse_config():
+    assert WebmConverter.parse_config("","low","on") == (
+                                    "-crf 20 -qmin 50 -qmax 100", 
+                                    "-c:a libopus"
+                                    )
+    assert WebmConverter.parse_config("","low","off") == (
+                                    "-crf 20 -qmin 50 -qmax 100", 
+                                    "-an"
+                                    )
+    assert WebmConverter.parse_config("","mid","on") == (
+                                    "-crf 15 -qmin 25 -qmax 75", 
+                                    "-c:a libopus"
+                                    )
+    assert WebmConverter.parse_config("","mid","off") == (
+                                    "-crf 15 -qmin 25 -qmax 75", 
+                                    "-an"
+                                    )
+    assert WebmConverter.parse_config("","high","on") == (
+                                    "-crf 10 -qmin 0 -qmax 50", 
+                                    "-c:a libopus"
+                                    )
+    assert WebmConverter.parse_config("","high","off") == (
+                                    "-crf 10 -qmin 0 -qmax 50", 
+                                    "-an"
+                                    )
+
 if __name__ == 'test_basic()':
     test_basic()
