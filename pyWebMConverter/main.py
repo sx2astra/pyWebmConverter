b425eee043e5a6813d586b26d155b30bc75fce2f
sx2astra <shsamuel92@gmail.com>
2022-12-18 23:01:15 -0700

    Getting Ready For Next Release

--- pyWebMConverter/main.py
+++ pyWebMConverter/main.py
@@ -54,9 +54,9 @@ def main():
 
     bitrate = the_converter.calculate_bitrate(file_size, video_duration)
     file_name = the_converter.set_file_name(file_name)
-    quality_setting, audio_setting = the_converter.parse_config(
-        quality_setting, audio_setting
-    )
+    #quality_setting, audio_setting = the_converter.parse_config(
+    #    quality_setting, audio_setting
+    #)
 
     print(f"{width} x {height} Video Resolution")
     print((bitrate), " KB/s")
@@ -69,49 +69,6 @@ def main():
         return 0
 
     video_pass = 1
-    """while video_pass <= 2:
-        subprocess.call(
-            [
-                "ffmpeg.exe",
-                "-i",
-                f"{input_video}",
-                "-c:v",
-                "libvpx-vp9",
-                "-pass",
-                f"{video_pass}",
-                "-b:v",
-                f"{bitrate}K",
-                "-threads",
-                "16",
-                "-speed",
-                "0",
-                f"{quality_setting}",
-                "-vf",
-                f"scale={width}:{height}",
-                "-tile-columns",
-                "0",
-                "-frame-parallel",
-                "0",
-                "-auto-alt-ref",
-                "1",
-                "-lag-in-frames",
-                "25",
-                "-row-mt",
-                "1",
-                "-g",
-                "600",
-                "-aq-mode",
-                "0",
-                f"{audio_setting}",
-                "-f",
-                "webm",
-                f".\Output\{file_name}",
-            ]
-        )
-        video_pass += 1"""
-
-
-    # Use this for now.
     while video_pass <= 2:
         subprocess.call(
             [
